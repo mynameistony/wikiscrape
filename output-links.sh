bb6fe6497e4be3390ba55a2751b426e046ae8bc7
Tony <tonyrogers10195@gmail.com>
2015-05-26 21:43:18 -0700

    Fixed some shit

--- output-links.sh
+++ output-links.sh
@@ -2,7 +2,7 @@
 archived=$(cat archived | sort | uniq | wc -l)
 unarchived=$(cat unarchived | sort | uniq | wc -l)
 
-echo "<p><b>$archived</b> archived | <b>$unarchived</b> waiting to be archived"
+echo "<p><b>$archived</b> <a href=/archived>archived</a> | <b>$unarchived</b> <a href=/unarchived>waiting to be archived</a>"
 for link in $(ls wiki)
 do
 	echo "<p><a href=/wiki/$link>$link</a></p>"
